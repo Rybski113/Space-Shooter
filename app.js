c2c49e427d041336720ce6ca492479cc4151a546
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2022-06-27 16:40:33 +0200

    moveShooter

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const grid = document.querySelector('.grid')
-let currentShooterIndex = 125
+let currentShooterIndex = 212
 
 
 for (let i = 0; i < 225; i++) {
@@ -10,3 +10,14 @@ for (let i = 0; i < 225; i++) {
 const squares = Array.from(document.querySelectorAll('.grid div'))
 
 squares[currentShooterIndex].classList.add('shooter')
+
+
+function moveShooter(e) {
+    squares[currentShooterIndex].classList.remove('shooter')
+
+    switch(e.key) {
+        case 'ArrowLeft':
+            if (currentShooterIndex % width !==0) currentShooterIndex
+    }
+    
+}
