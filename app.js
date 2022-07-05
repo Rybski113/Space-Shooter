29fa7bc0f330d4a51e9575910e5c9b4ed2c5e550
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2022-07-05 11:04:12 +0200

    Invaders Gone when Shoot

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ let goingRight = true
 let invadersId
 let score = 0
 
+let aliensRemoved = []
 
 
 
@@ -23,6 +24,27 @@ const squares = Array.from(document.querySelectorAll('.grid div'))
 squares[currentShooterIndex].classList.add('shooter')
 
 
+
+const alienInvaders = [
+    0,1,2,3,4,5,6,7,8,9,
+    15,16,17,18,19,20,21,22,23,24,
+    30,31,32,33,34,35,36,37,38,39
+]
+
+function draw() {
+    for (let i=0; i < alienInvaders.length; i ++)
+     if(!aliensRemoved.includes(i)){ 
+        squares[alienInvaders[i]].classList.add('invader')
+    }
+}
+draw()
+
+
+function remove() {
+    for (let i=0; i < alienInvaders.length; i++) {
+        squares[alienInvaders[i]].classList.remove('invader')
+    }
+}
 
 function moveShooter(e) {
     squares[currentShooterIndex].classList.remove('shooter')
@@ -47,25 +69,7 @@ document.addEventListener('keyup', moveShooter)
 
 
 
-const alienInvaders = [
-    0,1,2,3,4,5,6,7,8,9,
-    15,16,17,18,19,20,21,22,23,24,
-    30,31,32,33,34,35,36,37,38,39
-]
 
-function draw() {
-    for (let i=0; i < alienInvaders.length; i ++) {
-        squares[alienInvaders[i]].classList.add('invader')
-    }
-}
-draw()
-
-
-function remove() {
-    for (let i=0; i < alienInvaders.length; i++) {
-        squares[alienInvaders[i]].classList.remove('invader')
-    }
-}
 
 function moveInvaders() {
     const leftEdge = alienInvaders[0] % width === 0
@@ -99,16 +103,6 @@ function moveInvaders() {
             alert("GAME OVER!")
         }
     }
-
-    for( let i = 0; i < alienInvaders.length; i++) {
-        if(score === 34){
-            clearInterval(invadersId)
-            alert("YOU WON!")
-        }
-    }
-    
-
-   
     draw()
  
     }
@@ -129,8 +123,10 @@ function shoot(e) {
             squares[currentLaserIndex].classList.remove('invader')
             score++
             clearInterval(laserId)
-
             resultDisplay.innerHTML = score
+
+            const alienRemoved = alienInvaders.indexOf(currentLaserIndex)
+            aliensRemoved.push(alienRemoved)
         }
         
 
