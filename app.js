8c5173ef36a22c45937276353708504a33ff9e35
Norbert Piskorz <44465979+Rybski113@users.noreply.github.com>
2022-11-24 07:53:35 +0100

    ...

--- app.js
+++ app.js
@@ -1,93 +1,28 @@
-const grid = document.querySelector('.grid')
-let resultDisplay = document.getElementById('result')
-let invader =  document.querySelector('.invader')
-let currentShooterIndex = 202
-let width = 15
-let direction = 1
-let goingRight = true
+document.addEventListener('DOMContentLoaded', ()=> {
 
-let score = 0
-
-let aliensRemoved = []
+    const grid = document.getElementById('grid');
+    let platformCount = 5;
 
 
-
-
-for (let i = 0; i < 225; i++) {
-    const square = document.createElement('div')
-    grid.appendChild(square)
-}
-
-const squares = Array.from(document.querySelectorAll('.grid div'))
-
-squares[currentShooterIndex].classList.add('shooter')
-
-
-
-function moveShooter(e) {
-    squares[currentShooterIndex].classList.remove('shooter')
-
-    switch(e.key) {
-        case 'ArrowLeft':
-            if (currentShooterIndex % width !==0)
-             currentShooterIndex -=1
-            break
-        case 'ArrowRight':
-            if (currentShooterIndex % width < width -1) currentShooterIndex +=1
-            break    
+    class Platform {
+        constructor() {
+            this.bottom = newPlatformBottom;
+            this.left = Math.random() * 515;
+            
+        }
     }
 
-    squares[currentShooterIndex].classList.add('shooter')
-    
-}
-
-document.addEventListener('keyup', moveShooter)
-
-
-
-function shoot(e) {
-    let laserId
-    let currentLaserIndex = currentShooterIndex
-    function moveLaser() {
-        squares[currentLaserIndex].classList.remove('laser')
-        currentLaserIndex -= width
-        squares[currentLaserIndex].classList.add('laser')
+    function createplatforms() {
+        for (let i=0; i < platformCount; i++) {
+          let platformGap = 600 / platformCount;
+          let newPlatformBottom = 100 + i * platformGap;
+          let newPlatform = new Platform(newPlatformBottom);
+        }
     }
-    switch(e.key) {
-        case ' ':
-            laserId = setInterval(moveLaser, 200)
-     
+
+    function start() {
+        createplatforms()
     }
-}
-
-<<<<<<< HEAD
-
-document.addEventListener('keydown', shoot )
-
-function randomInvader() {
-    let randomSquare = squares[Math.floor(Math.random() * 15)]
-   randomSquare.classList.add('invader')
-   
-   
-   moveInvaders()
-
-}
-let showInvader = setInterval(randomInvader, 700)
-
-
-function moveInvaders() {
-   
-    let currentInvaderIndex = document.querySelectorAll('.invader')
-
-    
-    
-  
-    
-    console.log(currentInvaderIndex)
-
-}
-moveInvaders()
-
-=======
-document.addEventListener('keydown', shoot )
->>>>>>> a3727826686f55834c768e4825f3a06308aedf38
+   // button later
+    start()
+})
